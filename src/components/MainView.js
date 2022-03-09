b478980ffea976c9a7a899e68fb1c3dd1b97a7c1
CandelaOlha <olhacandela@gmail.com>
2022-03-09 12:24:46 -0300

    added global variables

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -5,14 +5,17 @@ const MainView = () => {
         <section>
             <MovieGrid
             title = "Upcoming Movies"
+            link = "upcoming-movies"
             category = "upcoming"
             />
             <MovieGrid
             title = "Top rated Movies"
+            link = "top-rated-movies"
             category = "top_rated"
             />
             <MovieGrid
             title = "Popular Movies"
+            link = "popular-movies"
             category = "popular"
             />
         </section>
--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -1,7 +1,21 @@
-const MovieGrid = ({title, category}) => {
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const MovieGrid = ({title, link, category}) => {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=9ac7847f0996d2c7efc0e9637bf8fd35&page=1`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, [])
+
     return (
-        <div>
-            <h2>{title}</h2>
+        <div className="movie-grid">
+            <div>
+                <h2>{title}</h2>
+            </div>
         </div>
     )
 }
