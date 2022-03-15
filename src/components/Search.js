f9534b316cf0cca348c4ba023cb7a9730811f285
CandelaOlha <olhacandela@gmail.com>
2022-03-15 12:26:13 -0300

    added search-bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,17 @@
+import "../styles/Search.scss";
+
 const Search = () => {
     return (
-        <div></div>
+        <main className="search-section">
+            <div className="search-section-content-container">
+                <form className="search-form">
+                    <label className="search-label">Search a movie
+                        <input type="text" placeholder="Type a title..." className="search-input"/>
+                    </label>
+                    <button className="search-button">Search</button>
+                </form>
+            </div>
+        </main>
     )
 }
 
