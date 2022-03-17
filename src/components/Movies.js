a21307f93a3ce619c510fbf1e9ebea20dd8c691e
CandelaOlha <olhacandela@gmail.com>
2022-03-17 15:28:14 -0300

    installed slick-carousel and created movie-slider component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,11 @@
 import "../styles/Movies.scss";
+import MoviesSlider from "./MoviesSlider";
 import MainView from "./MainView";
 
 const Movies = () => {
     return (
         <main className="movies">
+            <MoviesSlider />
             <MainView />
         </main>
     )
