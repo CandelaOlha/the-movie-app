af99c0cf1a9e9f2b8486d0fb43846e8fa9e280ee
CandelaOlha <olhacandela@gmail.com>
2022-03-30 14:24:54 -0300

    added styles to page-not-found

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
     return (
-        <main className="page-not-found-container">
-            <div className="image-container">
-                <img src={rex} alt="Rex from Toy Story looking worried about not finding the page"/>
-            </div>
-            <div className="text-container">
-                <h2 className="main-title">Oops! Page not found.</h2>
-                <p className="description">We couldn't find the page you are looking for.  But don't worry! You can return to the previous page or to the homepage.</p>
-                <Link to="/" className="homepage-link">Go to homepage</Link>
+        <main className="page-not-found">
+            <div className="page-not-found-container">
+                <div className="image-container">
+                    <img src={rex} alt="Rex from Toy Story looking worried about not finding the page"/>
+                </div>
+                <div className="text-container">
+                    <h2 className="main-title">Oops! Page not found.</h2>
+                    <p className="description">We couldn't find the page you are looking for.  But don't worry! You can return to the previous page or to the homepage.</p>
+                    <Link to="/" className="homepage-link">Go to homepage</Link>
+                </div>
             </div>
         </main>
     )
