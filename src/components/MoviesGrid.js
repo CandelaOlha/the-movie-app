4bdb97f75b7dd029f7884334970332255d8a8cf4
CandelaOlha <olhacandela@gmail.com>
2022-03-31 14:55:23 -0300

    deleted unnecessary breakpoints and styles

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -19,24 +19,17 @@ const MoviesGrid = ({url, link, title}) => {
         initialSlide: 0,
         responsive: [
             {
-                breakpoint: 1094,
+                breakpoint: 1024,
                 settings: {
                 slidesToShow: 4,
                 slidesToScroll: 4
-                }
-              },
-            {
-                breakpoint: 1024,
-                settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3
                 }
             },
             {
                 breakpoint: 700,
                 settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToShow: 3,
+                slidesToScroll: 3,
                 }
             },
             {
