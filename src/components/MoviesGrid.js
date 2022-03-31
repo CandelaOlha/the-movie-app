4c14bd7732ca499ff41b7b71038e82fcd02fe7c8
CandelaOlha <olhacandela@gmail.com>
2022-03-31 12:07:18 -0300

    added more styles to the movie carousel

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -12,37 +12,41 @@ const MoviesGrid = ({url, link, title}) => {
 
     const settings = {
         dots: false,
-        infinite: false,
+        infinite: true,
         speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         initialSlide: 0,
         responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
+            {
+                breakpoint: 1094,
+                settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+                }
+              },
+            {
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+                }
             }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
+            ]
       };
 
     return (
