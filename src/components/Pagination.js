b966b08ef43ad281452e54664c8a7206ab1a6e1d
CandelaOlha <olhacandela@gmail.com>
2022-04-03 19:45:49 -0300

    added pagination component

---
+++ src/components/Pagination.js
@@ -0,0 +1,23 @@
+import "../styles/Pagination.scss";
+import { MdFirstPage, MdNavigateBefore, MdNavigateNext, MdLastPage } from 'react-icons/Md';
+
+const Pagination = () => {
+    return (
+        <div className="pagination">
+            <button>
+                <MdFirstPage />
+            </button>
+            <button>
+                <MdNavigateBefore />
+            </button>
+            <button>
+                <MdNavigateNext />
+            </button>
+            <button>
+                <MdLastPage />
+            </button>
+        </div>
+    )
+}
+
+export default Pagination;
